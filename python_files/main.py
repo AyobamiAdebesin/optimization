10d68b9c086e9d69c47b3efc10b6923c2e304151
AyobamiAdebesin <froshofficial@gmail.com>
2024-02-05 22:51:02 -0500

    Renamed reference to file after name change

--- python_files/main.py
+++ python_files/main.py
@@ -2,11 +2,11 @@
 import os
 import sys
 from linear_least_squares import linear_least_squares, plot_points
-from exp_least_squares import exponential_least_squares, plot_exp_points
+from python_files.least_squares import least_squares, plot_exp_points
 from generate_data import generate_synthetic_data
 
 
 data_points = generate_synthetic_data()
-alpha, C = exponential_least_squares(data_points)
+alpha, C = least_squares(data_points)
 print(f"C: {C}, alpha: {alpha}")
 plot_exp_points(data_points, alpha, C)
