383f0be91e5ac4f08f9398aaf8335a5cde69ec24
AyobamiAdebesin <froshofficial@gmail.com>
2024-02-05 22:54:33 -0500

    Updated import statement

--- python_files/main.py
+++ python_files/main.py
@@ -2,7 +2,7 @@
 import os
 import sys
 from linear_least_squares import linear_least_squares, plot_points
-from python_files.least_squares import least_squares, plot_exp_points
+from least_squares import least_squares, plot_exp_points
 from generate_data import generate_synthetic_data
 
 
